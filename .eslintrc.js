424fec37c5f751e0d6c2abc2585a7612a77db11f
bowenlee <1207107376@qq.com>
2020-06-14 23:34:07 +0800

    feat(代码检测):添加项目eslint检测

---
+++ .eslintrc.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const path = require('path');
+const root = __dirname;
+
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    es6: true,
+    mocha: true,
+    jest: true,
+    jasmine: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ].map(function (key) {
+    return require.resolve('eslint-config-' + key);
+  }),
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'eslint-comments', 'jest', 'unicorn', 'react-hooks'],
+  rules: {
+    'react/jsx-wrap-multilines': 0,
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/sort-comp': 1,
+    'react/jsx-one-expression-per-line': 0,
+    'generator-star-spacing': 0,
+    'function-paren-newline': 0,
+    'import/no-unresolved': [2, { ignore: ['^@/', '^umi/'] }],
+    'import/order': 'warn',
+    'import/no-extraneous-dependencies': [
+      2,
+      {
+        optionalDependencies: true,
+        devDependencies: [
+          '**/tests/**.{ts,js,jsx,tsx}',
+          '**/_test_/**.{ts,js,jsx,tsx}',
+          '/mock/**/**.{ts,js,jsx,tsx}',
+          '**/**.test.{ts,js,jsx,tsx}',
+          '**/_mock.{ts,js,jsx,tsx}',
+          '**/example/**.{ts,js,jsx,tsx}',
+          '**/examples/**.{ts,js,jsx,tsx}',
+        ],
+      },
+    ],
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'linebreak-style': 0,
+    'no-prototype-builtins': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': [0, 'camel-case'],
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': 'off',
+    'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
+    '@typescript-eslint/explicit-function-return-type': [
+      'off',
+      { allowTypedFunctionExpressions: true },
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
+    '@typescript-eslint/no-var-requires': 0,
+    'unicorn/prevent-abbreviations': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    'import/no-cycle': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'off',
+    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-parens': 1,
+    'object-curly-newline': 0,
+    'implicit-arrow-linebreak': 0,
+    'operator-linebreak': 0,
+    'eslint-comments/no-unlimited-disable': 1,
+    'no-param-reassign': 1,
+    'space-before-function-paren': 0,
+    'import/extensions': 0,
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: path.resolve(root, 'config/webpack.config.js'),
+      },
+      node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    },
+    polyfills: ['fetch', 'Promise', 'URL', 'object-assign'],
+  },
+};
